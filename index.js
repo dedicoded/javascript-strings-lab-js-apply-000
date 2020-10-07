87b37111e75b7410b51d17b9a75f964fc3dc27f8
Louis Joe <newhopemissionliberia@gmail.com>
2020-10-07 00:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about";
+var topic = "";
 
-var conversation = `${topic}`;
+var conversation = `${Let's talk about}`;
 
 
